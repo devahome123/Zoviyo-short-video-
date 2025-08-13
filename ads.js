751f45d20177717663fc5a437c63c9ab4bb7023c
Deepak yadav <dgope4244@gmail.com>
2025-08-13 15:37:48 +0530

    Delete ads.js

--- ads.js
+++
@@ -1,8 +0,0 @@
-// routes/ads.js
-const express = require('express');
-const router = express.Router();
-const { getAdConfig } = require('../controllers/adController');
-
-router.get('/config', getAdConfig);
-
-module.exports = router;
