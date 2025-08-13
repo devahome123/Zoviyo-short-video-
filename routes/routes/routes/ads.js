17434414d196f5fe80015805b114e780e5df03f0
Deepak yadav <dgope4244@gmail.com>
2025-08-13 15:05:18 +0530

    Create ads.js

---
+++ routes/routes/routes/ads.js
@@ -0,0 +1,9 @@
+// routes/ads.js
+
+const express = require('express');
+const router = express.Router();
+const { getAdConfig } = require('../controllers/adController');
+
+router.get('/config', getAdConfig);
+
+module.js
